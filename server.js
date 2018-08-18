4a958b29278a13b9bdcb3d06c98899d1fcbead52
Neha Pathak <neha.pathak@hotmail.com>
2018-08-18 14:00:39 -0400

    fixed the printing

--- server.js
+++ server.js
@@ -8,72 +8,41 @@ var PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-<<<<<<< HEAD
-var reservations = [];
-var waitList = [];
-
-app.get("/api/tables", function (req, res) {
-    return res.json(reservations)
-});
-=======
 var orders = [];
 
 var waitlist = [];
 
->>>>>>> c7a7fc5869d76149bf1421145d646dffdaafc5cb
 
 app.get("/api/reservations", function (req, res) {
-    return res.json(reservations)
+  return res.json(reservations)
 });
 
-<<<<<<< HEAD
-
-app.get("/api/waitlist", function (req, res) {
-    return res.json(waitList)
-});
-
-app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "reservation.html"));
-  });
-
-  app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "resevation.html"));
-  });
-
-
-app.post("/reserve", function(req, res) {
-    var newReservation = req.body;
-  
-    console.log(newReservation);
-  
-    characters.push(newReservation);
-  });
-  
-  
-=======
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   return res.json(orders)
 });
 
-app.get("/tables", function(req, res) {
+app.get("/tables", function (req, res) {
   res.sendFile(path.join(__dirname, "/tables.html"));
 });
 
-app.get("/reserve", function(req, res) {
+app.get("/reserve", function (req, res) {
   res.sendFile(path.join(__dirname, "/reservation.html"));
 });
 
-app.get("/api/tables", function(req, res) {
+app.get("/api/tables", function (req, res) {
   return res.json(orders);
 });
 
-app.get("/api/reserve", function(req, res) {
+app.get("/api/reserve", function (req, res) {
   return res.json(waitlist)
 });
 
-app.post("/api/tables", function(req, res) {
+app.post("/api/tables", function (req, res) {
   var newOrder = req.body;
->>>>>>> c7a7fc5869d76149bf1421145d646dffdaafc5cb
+  newOrder.customerName = newOrder.customerName;
+  newOrder.phoneNumber = newOrder.phoneNumber;
+  newOrder.customerEmail = newOrder.customerEmail;
+  newOrder.customerID = newOrder.customerID;
 
   if (orders.length <= 2) {
     orders.push(newOrder)
@@ -85,6 +54,6 @@ app.post("/api/tables", function(req, res) {
 });
 
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
